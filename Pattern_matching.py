9ee0cda66f1d0aeee2460c275f7c498508cfc460
ztrend <zoran.pantoulas@gmail.com>
2023-06-01 14:57:02 +0200

    Changed the algorithm to linear time complexity, as the previous one had quadratic time complexity

--- Pattern_matching.py
+++ Pattern_matching.py
@@ -1,4 +1,7 @@
-"""Write a function PatternMatching that solves the Pattern Matching Problem. Then add PatternMatching to Replication.py.
+"""Write a function PatternMatching that solves the Pattern Matching Problem.
+Pattern Matching Problem: Find all occurrences of a pattern in a string.
+     Input: Strings Pattern and Genome.
+     Output: All starting positions in Genome where Pattern appears as a substring.
 Sample Input:
 ATAT
 GATATATGCATATACTT
@@ -9,17 +12,29 @@ Sample Output:
 import sys
 
 def PatternMatching(Pattern, Genome):
-    positions = [] # output variable
-    a =0
-    for i in range(len(Genome)):
-        #This will find the first occurence of the pattern starting from the position a+i. the first attempt will start from 0.
-        # Why a+i? x tells us where the first occurence starts. So in the next iteration we don't care to go over all the letters. we just want to go one step after the first occurence.
-        # That will be i + a (which will equal to x)
-        x = Genome.find(Pattern, a+i, len(Genome))
-        if x != -1:
-            positions.append(x)
-            a = x  #
+    positions = []
+    pat = len(Pattern)
+    for i in range(len(Genome) - pat + 1):  # +1 to include the end
+        if Genome[i:i+pat] == Pattern:  # Slice Genome to get a substring of the same length as Pattern
+            positions.append(i)
     return positions
+
+
+
+
+#This function has quadratic time complexity because find will be iterated over the whole sequence multiple times.
+# def PatternMatching(Pattern, Genome):
+#     positions = [] # output variable
+#     a =0
+#     for i in range(len(Genome)):
+#         #This will find the first occurence of the pattern starting from the position a+i. the first attempt will start from 0.
+#         # Why a+i? x tells us where the first occurence starts. So in the next iteration we don't care to go over all the letters. we just want to go one step after the first occurence.
+#         # That will be i + a (which will equal to x)
+#         x = Genome.find(Pattern, a+i, len(Genome))
+#         if x != -1:
+#             positions.append(x)
+#             a = x  #
+#     return positions
 
 def main():
     if len(sys.argv) != 3:
